ac2ae646f8f74fd80b4d908d39c38c873e8db532
Pavel <74056595+Pavel775@users.noreply.github.com>
2026-03-03 21:04:02 +0100

    Update registrar.js

--- api/registrar.js
+++ api/registrar.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_ANON_KEY
 )
 
 export default async function handler(req, res) {
